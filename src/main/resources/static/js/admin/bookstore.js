831732ab0c2ee48cb3c9caebb9e04528d61c460e
WWIT <poiut5073@gmail.com>
2021-07-11 00:10:06 +0900

    bookstamp css 수정완료

--- src/main/resources/static/js/admin/bookstore.js
+++ src/main/resources/static/js/admin/bookstore.js
@@ -1,27 +1,35 @@
 //팝업주소, 팝업창 이름, 팝업창 설정
 
 window.addEventListener("load", function(){
-	console.log("text");
+	
 	let card = document.querySelector(".card");
 	let popupBtn = document.querySelector(".popup-btn");
 	let popup = document.querySelector(".popup");
 	let btnList = document.querySelector(".btn-list");
+	let assignBtn = document.querySelector(".assign-btn");
 	
 	
-	card.addEventListener("click", function(e){
+	card.addEventListener("click", (e) =>{
 		e.preventDefault();
 		
-		console.log("11");
 		if(!e.target.classList.contains("popup-btn"))
 			return;
 			
 		popup.classList.remove("d-none");
-		
-		// 클로즈 버튼 클릭하면 d-none 추가하기
-		
-
-				
-			})
+			
 	})
+
+	let input = document.querySelectorAll("input[type=hidden]");
+	let bookstoreId = input[0]
+	let MemberId = input[1]
+	assignBtn.addEventListener("click",(e) =>{
+		
+		
+		
+	})
+
+
+		
+})
 	
 	
