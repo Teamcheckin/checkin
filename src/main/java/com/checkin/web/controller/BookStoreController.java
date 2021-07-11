93417aa1bca039003badb200cbf26bc25038e3c2
damy <ekamiss22@gmail.com>
2021-07-11 14:50:19 +0900

    location submit

--- src/main/java/com/checkin/web/controller/BookStoreController.java
+++ src/main/java/com/checkin/web/controller/BookStoreController.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.checkin.web.entity.BookStore;
 import com.checkin.web.entity.BookStoreView;
 import com.checkin.web.entity.ReviewView2;
 import com.checkin.web.service.BookStoreService;
@@ -37,4 +41,25 @@ public class BookStoreController {
 		return "bookstore/detail";
 	}
 	
+	@PostMapping("detail/location")
+	public String detailLocation(
+					@RequestParam(required=false) Double latitude,
+					@RequestParam(required=false) Double longitude,
+					@RequestParam(name="bookstoreId") Integer id,
+					Model model, RedirectAttributes re) {
+		
+		BookStore bookstore = service.get(id);
+		List<ReviewView2> review = reviewService.getBookStoreList(id);
+		bookstore.setLatitude(latitude);
+		bookstore.setLongitude(longitude);
+		service.update(bookstore);
+		System.out.println(bookstore);
+		BookStoreView bookstoreView = service.getView(id);
+		
+		re.addAttribute("id", id);
+		model.addAttribute("bookstore", bookstoreView);
+		model.addAttribute("review", review);
+		
+		return "redirect:/bookstore/detail/{id}";
+	}
 }
