ebf9d0464e8ddce647d4c84e6ec6611f636ee2e2
minjudev <aamj0226@gmail.com>
2021-07-23 15:29:14 +0900

    Update setting

--- src/main/java/com/checkin/web/dao/mybatis/MyBatisBookStoreDao.java
+++ src/main/java/com/checkin/web/dao/mybatis/MyBatisBookStoreDao.java
@@ -2,7 +2,6 @@ package com.checkin.web.dao.mybatis;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.SqlSession;
  
 import org.springframework.beans.factory.annotation.Autowired;
