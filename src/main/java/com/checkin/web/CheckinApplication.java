b2417d48b88624902b6fc063a18c1fac5f6bbdd6
minjudev <aamj0226@gmail.com>
2021-07-29 21:44:35 +0900

    Update

--- src/main/java/com/checkin/web/CheckinApplication.java
+++ src/main/java/com/checkin/web/CheckinApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "com.checkin.web.*" })
 public class CheckinApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/checkin/web/dao/mybatis/MyBatisBookStoreDao.java
+++ src/main/java/com/checkin/web/dao/mybatis/MyBatisBookStoreDao.java
@@ -3,8 +3,7 @@ package com.checkin.web.dao.mybatis;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
- 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Repository;
 
  
